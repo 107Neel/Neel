caa14355d867fda8f0cf028bbaad3476200ba7e4
107Neel <88573940+107Neel@users.noreply.github.com>
2022-04-14 21:17:30 +0530

    Add files via upload

---
+++ regex2.js
@@ -0,0 +1,3 @@
+const obj = /e/.exec("The best things in life are free!");
+document.getElementById("demo").innerHTML =
+"Found " + obj[0] + " in position " + obj.index + " in the text: " + obj.input;
